abee6f2db4cbf5f05538a4bf378d6a93a80ea3a6
cat <cooldiamond461@gmail.com>
2026-07-24 22:11:02 +0000

    hi i added colors to topbar

--- webpack.config.js
+++ webpack.config.js
@@ -168,27 +168,27 @@ module.exports = [
             }),
             new HtmlWebpackPlugin({
                 chunks: ['editor'], template: 'src/playground/index.ejs', filename: 'editor.html',
-                title: 'PenguinMod - Editor', ...htmlWebpackPluginCommon
+                title: 'super very cool editor from zzPurrzw', ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['playground'], template: 'src/playground/index.ejs', filename: 'playground.html',
-                title: 'PenguinMod - Playground', ...htmlWebpackPluginCommon
+                title: 'zzPurrzw very cool playground', ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['player'], template: 'src/playground/index.ejs', filename: 'index.html',
-                title: 'PenguinMod - A mod of TurboWarp', ...htmlWebpackPluginCommon
+                title: 'zzPurrzw - A mod of PenguinMod', ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['fullscreen'], template: 'src/playground/index.ejs', filename: 'fullscreen.html',
-                title: 'PenguinMod - A mod of TurboWarp', ...htmlWebpackPluginCommon
+                title: 'zzPurrzw - A mod of PenguinMod', ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['embed'], template: 'src/playground/index.ejs', filename: 'embed.html',
-                title: 'Embedded Project - PenguinMod', noTheme: true, ...htmlWebpackPluginCommon
+                title: 'embedded project - zzPurrzw', noTheme: true, ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['addon-settings'], template: 'src/playground/simple.ejs', filename: 'addons.html',
-                title: 'Addon Settings - PenguinMod', ...htmlWebpackPluginCommon
+                title: 'addon settings - zzpurrzw', ...htmlWebpackPluginCommon
             }),
             new HtmlWebpackPlugin({
                 chunks: ['credits'], template: 'src/playground/simple.ejs', filename: 'credits.html',
