f78f2a5fb8a66352612bdf1d45ce58df13eebbba
cat <cooldiamond461@gmail.com>
2026-07-25 01:33:50 +0000

    i hope i finally corrected purr

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -3,7 +3,7 @@ import projectData from './project-data';
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./purr.svg';
+import costume1 from '!arraybuffer-loader!./Purr.png';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
@@ -37,9 +37,9 @@ const defaultProject = translator => {
         data: encoder.encode(backdrop)
     }, {
         id: 'c434b674f2da18ba13cdfe51dbc05ecc',
-        assetType: 'ImageVector',
-        dataFormat: 'SVG',
-        data: encoder.encode(costume1)
+        assetType: 'ImageBitmap',
+        dataFormat: 'png',
+        data: new Uint8Array(costume1)
     }];
 };
 export default defaultProject;
