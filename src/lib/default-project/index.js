b0fac4b1f0aa47e13a955c3b71ce78d352077d1a
cat <cooldiamond461@gmail.com>
2026-07-25 01:11:20 +0000

    Purr is here!

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -3,7 +3,7 @@ import projectData from './project-data';
 /* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./zzpurrzw-costume.svg';
+import costume1 from '!raw-loader!./Purr.png';
 /* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
